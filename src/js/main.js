8e45a08e97ec16e5c198394b8b18a7ea3b635ba2
Tomoya Koyanagi <tomk79@gmail.com>
2022-11-06 13:40:55 +0900

    編集中のテーブルのプレビューを表示するようになった。

--- src/js/main.js
+++ src/js/main.js
@@ -10,13 +10,39 @@ module.exports = function( elm ){
 	var $elms = {
 		"main": $(templates.mainframe()),
 		"targetTextarea": $(elm),
+		"srcEditor": null,
+		"previewTable": null,
 	};
 
+	function updatePreview(){
+		$elms.previewTable.html( $elms.srcEditor.val() );
+	}
+
+	function save(){
+		$elms.targetTextarea.val( $elms.srcEditor.val() );
+	}
 
 	// Initialize
 	new Promise(function(rlv){rlv();})
 		.then(function(){ return new Promise(function(rlv, rjt){
 			$elms.targetTextarea.before( $elms.main );
+
+			$elms.srcEditor = $elms.main.find('textarea.table-tag-editor__src');
+			$elms.previewTable = $elms.main.find('table.table-tag-editor__visual-editor-table');
+
+			$elms.srcEditor.val( $elms.targetTextarea.val() );
+			updatePreview();
+			rlv();
+		}); })
+		.then(function(){ return new Promise(function(rlv, rjt){
+			var textareaChangedTimer;
+			$elms.srcEditor.on('change.table-tag-editor keyup.table-tag-editor', function(){
+				clearTimeout( textareaChangedTimer );
+				textareaChangedTimer = setTimeout(function(){
+					save();
+					updatePreview();
+				}, 200);
+			});
 			rlv();
 		}); })
 	;
